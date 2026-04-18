9ec7481d6cf5a1834250cef569c119829eca0882
gerard1210 <gerard.saulnier@gmail.com>
2026-04-18 13:14:20 +0200

    Creation de la classFirst

---
+++ src/fisrt/ClassFirst.java
@@ -0,0 +1,16 @@
+package fisrt;
+
+public class ClassFirst {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
